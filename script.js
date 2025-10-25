9af7c9bbedc290b22758ebab501e42b38f16d912
leandrorodca <leandrorodca@gmail.com>
2025-10-25 12:27:08 -0300

    alterando script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Executa o código quando a página é totalmente carregada
 window.onload = function() {
     // Exibe um alerta
-    alert("Bem-vindo à página!");
+    alert("Bem-vindo ao meu site!");
 };
